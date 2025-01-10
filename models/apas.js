e8f0acd1349f446e1c8c06ad1c532bbc37e49c5c
Juan Brusco <juanbrusco_23@hotmail.com>
2025-01-10 16:16:51 -0300

    apikey

--- models/apas.js
+++ models/apas.js
@@ -1,7 +1,15 @@
+const sqlite3 = require("sqlite3").verbose();
+const { Database } = require("@sqlitecloud/drivers");
+const dotenv = require("dotenv");
+dotenv.config();
+const apikey = process.env.SQLITECLOUD;
+const db = new Database(
+  "sqlitecloud://cijmwjfdhz.g2.sqlite.cloud:8860/apas.db?apikey=" + apikey
+);
+module.exports = db;
+
 // const sqlite3 = require("sqlite3").verbose();
-// const { Database } = require('@sqlitecloud/drivers');
-// const connectionString = 'sqlitecloud://chbjkzyihk.sqlite.cloud:8860/apas?apikey=ZUo3zLgRLtXRESBYsKHX557Nrq1wdHyDaKnfLkBmGyg';
-// let db = new sqlite3.Database(connectionString, (err) => {
+// let db = new sqlite3.Database("./db/apas.db", (err) => {
 //   if (err) {
 //     console.log("DB connection ERROR.");
 //     console.error(err.message);
@@ -9,13 +17,3 @@
 //   console.log("DB connection OK.");
 // });
 // module.exports = db;
-
-const sqlite3 = require("sqlite3").verbose();
-let db = new sqlite3.Database("./db/apas.db", (err) => {
-  if (err) {
-    console.log("DB connection ERROR.");
-    console.error(err.message);
-  }
-  console.log("DB connection OK.");
-});
-module.exports = db;
